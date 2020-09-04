2b881a9d0a084ffeb440b1d08d673fd481742b09
Kirill Rozov <krl.rozov@gmail.com>
2020-09-05 00:24:26 +0300

    Add possibility to clear ViewBindingProperty manually

--- library/src/main/java/by/kirich1409/viewbindingdelegate/ViewBindingProperty.kt
+++ library/src/main/java/by/kirich1409/viewbindingdelegate/ViewBindingProperty.kt
@@ -2,20 +2,11 @@ package by.kirich1409.viewbindingdelegate
 
 import android.os.Handler
 import android.os.Looper
-import android.view.View
-import androidx.annotation.IdRes
 import androidx.annotation.MainThread
-import androidx.core.app.ComponentActivity
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
-import by.kirich1409.viewbindingdelegate.internal.ActivityViewBinder
-import by.kirich1409.viewbindingdelegate.internal.DialogFragmentViewBinder
-import by.kirich1409.viewbindingdelegate.internal.FragmentViewBinder
 import by.kirich1409.viewbindingdelegate.internal.checkIsMainThread
-import by.kirich1409.viewbindingdelegate.internal.requireViewByIdCompat
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -23,9 +14,8 @@ abstract class ViewBindingProperty<in R : Any, T : ViewBinding>(
     private val viewBinder: (R) -> T
 ) : ReadOnlyProperty<R, T> {
 
-    internal var viewBinding: T? = null
-    private val lifecycleObserver = BindingLifecycleObserver()
-
+    private var viewBinding: T? = null
+    private val lifecycleObserver = ClearOnDestroyLifecycleObserver()
     private var thisRef: R? = null
 
     protected abstract fun getLifecycleOwner(thisRef: R): LifecycleOwner
@@ -43,16 +33,26 @@ abstract class ViewBindingProperty<in R : Any, T : ViewBinding>(
         return viewBinder(thisRef).also { viewBinding = it }
     }
 
-    private inner class BindingLifecycleObserver : DefaultLifecycleObserver {
+    @MainThread
+    public fun clear() {
+        checkIsMainThread()
 
-        private val mainHandler = Handler(Looper.getMainLooper())
+        val thisRef = thisRef ?: return
+        this.thisRef = null
+        getLifecycleOwner(thisRef).lifecycle.removeObserver(lifecycleObserver)
+        mainHandler.post {
+            viewBinding = null
+        }
+    }
+
+    private inner class ClearOnDestroyLifecycleObserver : DefaultLifecycleObserver {
 
         @MainThread
-        override fun onDestroy(owner: LifecycleOwner) {
-            owner.lifecycle.removeObserver(this)
-            mainHandler.post {
-                viewBinding = null
-            }
-        }
+        override fun onDestroy(owner: LifecycleOwner): Unit = clear()
+    }
+
+    private companion object {
+
+        private val mainHandler = Handler(Looper.getMainLooper())
     }
 }
