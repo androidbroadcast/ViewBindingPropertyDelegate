e13eff3dc877fdbb081f04d28a378cea2b3ec42f
Kirill Rozov <krl.rozov@gmail.com>
2020-10-22 00:24:59 +0300

    Add sample with inflate for DialogFragment

---
+++ sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/inflate/ProfileDialogFragment1.kt
@@ -0,0 +1,24 @@
+@file:Suppress("unused")
+
+package by.kirich1409.viewbindingdelegate.sample.reflection.inflate
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.dialogViewBinding
+import by.kirich1409.viewbindingdelegate.sample.R
+import by.kirich1409.viewbindingdelegate.sample.databinding.FragmentProfileBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
+
+class ProfileDialogFragment1 : DialogFragment() {
+
+    private val viewBinding: FragmentProfileBinding by viewBinding(createMethod = CreateMethod.INFLATE)
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(requireContext())
+            .setView(viewBinding.root)
+            .create()
+    }
+}
