753634a16fcd9094afe718c7f4f5ac34723b8e79
Kirill Rozov <krl.rozov@gmail.com>
2020-12-31 00:04:54 +0300

    Update sample

--- sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/inflate/ProfileActivity.kt
+++ sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/inflate/ProfileActivity.kt
@@ -4,12 +4,13 @@ package by.kirich1409.viewbindingdelegate.sample.reflection.inflate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.sample.databinding.ActivityProfileBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
 
 class ProfileActivity : AppCompatActivity() {
 
-    private val viewBindingUsingReflection: ActivityProfileBinding by viewBinding()
+    private val viewBindingUsingReflection: ActivityProfileBinding by viewBinding(CreateMethod.INFLATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
