64912025f45e06bc94e0037581ad66a13922a66b
Kirill Rozov <krl.rozov@gmail.com>
2020-12-31 12:50:11 +0300

    Optimize LifecycleViewBindingProperty

--- vbpd/vbpd-noreflection/src/main/java/by/kirich1409/viewbindingdelegate/ViewBindingProperty.kt
+++ vbpd/vbpd-noreflection/src/main/java/by/kirich1409/viewbindingdelegate/ViewBindingProperty.kt
@@ -58,9 +58,7 @@ public abstract class LifecycleViewBindingProperty<in R : Any, T : ViewBinding> 
         val lifecycle = getLifecycleOwner(thisRef).lifecycle
         val viewBinding = viewBinder(thisRef)
         if (lifecycle.currentState == Lifecycle.State.DESTROYED) {
-            mainHandler.post { this.viewBinding = null }
-            // We can access to ViewBinding after on destroy,
-            // but don't save it in property to prevent memory leak
+            // We can access to ViewBinding after on destroy, but don't save to prevent memory leak
         } else {
             lifecycle.addObserver(lifecycleObserver)
             this.viewBinding = viewBinding
