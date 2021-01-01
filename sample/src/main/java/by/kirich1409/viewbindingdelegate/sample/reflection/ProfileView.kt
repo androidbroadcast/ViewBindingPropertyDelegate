f084ac95d8dada4b85e2aad9fd06957c590e7a3a
Kirill Rozov <krl.rozov@gmail.com>
2021-01-01 19:01:56 +0300

    Update sample for ViewGroup

--- sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/ProfileView.kt
+++ sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/ProfileView.kt
@@ -3,17 +3,27 @@
 package by.kirich1409.viewbindingdelegate.sample.reflection
 
 import android.content.Context
-import android.widget.FrameLayout
+import android.widget.LinearLayout
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.sample.R
 import by.kirich1409.viewbindingdelegate.sample.databinding.ViewProfileBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
 
-class ProfileView(context: Context) : FrameLayout(context) {
+class ProfileView(context: Context) : LinearLayout(context) {
 
     init {
+        orientation = VERTICAL
         inflate(context, R.layout.view_profile, this)
     }
 
     private val viewBinding: ViewProfileBinding by viewBinding(CreateMethod.BIND)
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        // ViewBinding views be added only after first call of viewBinding, because layout resource contains <merge> in the root
+        with(viewBinding) {
+            name.text = "Name"
+            status.text = "Status"
+        }
+    }
 }
--- sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/inflate/ProfileView.kt
+++ sample/src/main/java/by/kirich1409/viewbindingdelegate/sample/reflection/inflate/ProfileView.kt
@@ -3,17 +3,31 @@
 package by.kirich1409.viewbindingdelegate.sample.reflection.inflate
 
 import android.content.Context
-import android.widget.FrameLayout
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import androidx.annotation.AttrRes
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.sample.databinding.ViewProfileBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
 
-class ProfileView(context: Context) : FrameLayout(context) {
+class ProfileView @JvmOverloads constructor(
+    context: Context,
+    atts: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : LinearLayout(context, atts, defStyleAttr) {
+
+    init {
+        orientation = VERTICAL
+    }
 
     private val viewBinding: ViewProfileBinding by viewBinding(CreateMethod.INFLATE)
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        addView(viewBinding.root)
+        // ViewBinding views be added only after first call of viewBinding, because layout resource contains <merge> in the root
+        with(viewBinding) {
+            name.text = "Name"
+            status.text = "Status"
+        }
     }
 }
