1f5433cd9cdf5be4cf42b1e1560c9942be615de7
Kirill Rozov <krl.rozov@gmail.com>
2021-01-01 12:41:27 +0300

    Start use cached reflection call

--- vbpd/vbpd-full/src/main/java/by/kirich1409/viewbindingdelegate/ViewGroupBindings.kt
+++ vbpd/vbpd-full/src/main/java/by/kirich1409/viewbindingdelegate/ViewGroupBindings.kt
@@ -3,30 +3,9 @@
 
 package by.kirich1409.viewbindingdelegate
 
-import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RestrictTo
 import androidx.viewbinding.ViewBinding
-
-@RestrictTo(RestrictTo.Scope.LIBRARY)
-@PublishedApi
-internal class ViewGroupBinder<T : ViewBinding>(private val viewBindingClass: Class<T>) {
-
-    /**
-     * Cache static method `ViewBinding.bind(View)`
-     */
-    private val bindViewMethod by lazy(LazyThreadSafetyMode.NONE) {
-        viewBindingClass.getMethod("bind", View::class.java)
-    }
-
-    /**
-     * Create new [ViewBinding] instance
-     */
-    @Suppress("UNCHECKED_CAST")
-    fun bind(viewGroup: ViewGroup): T {
-        return bindViewMethod(null, viewGroup) as T
-    }
-}
+import by.kirich1409.viewbindingdelegate.internal.ViewBindingCache
 
 /**
  * Create new [ViewBinding] associated with the [ViewGroup]
@@ -34,9 +13,8 @@ internal class ViewGroupBinder<T : ViewBinding>(private val viewBindingClass: Cl
  * @param T Class of expected [ViewBinding] result class
  */
 @JvmName("viewBindingFragment")
-public inline fun <reified T : ViewBinding> ViewGroup.viewBinding(
-): ViewBindingProperty<ViewGroup, T> {
-    return viewBinding(ViewGroupBinder(T::class.java)::bind)
+public inline fun <reified T : ViewBinding> ViewGroup.viewBinding(): ViewBindingProperty<ViewGroup, T> {
+    return viewBinding(T::class.java)
 }
 
 /**
@@ -45,8 +23,6 @@ public inline fun <reified T : ViewBinding> ViewGroup.viewBinding(
  * @param viewBindingClass Class of expected [ViewBinding] result class
  */
 @JvmName("viewBindingFragment")
-public fun <T : ViewBinding> ViewGroup.viewBinding(
-    viewBindingClass: Class<T>,
-): ViewBindingProperty<ViewGroup, T> {
-    return viewBinding(ViewGroupBinder(viewBindingClass)::bind)
+public fun <T : ViewBinding> ViewGroup.viewBinding(viewBindingClass: Class<T>, ): ViewBindingProperty<ViewGroup, T> {
+    return viewBinding { ViewBindingCache.getBind(viewBindingClass).bind(this) }
 }
