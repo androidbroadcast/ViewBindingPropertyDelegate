3b4449bf189485fb506fe93effd503d86cda2264
Kirill Rozov <krl.rozov@gmail.com>
2021-01-01 18:59:37 +0300

    Fix bugs

--- vbpd/vbpd-full/src/main/java/by/kirich1409/viewbindingdelegate/internal/ViewBindingCache.kt
+++ vbpd/vbpd-full/src/main/java/by/kirich1409/viewbindingdelegate/internal/ViewBindingCache.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.annotation.RestrictTo
 import androidx.annotation.RestrictTo.Scope.LIBRARY
 import androidx.viewbinding.ViewBinding
+import java.lang.reflect.Method
 
 object ViewBindingCache {
 
@@ -17,9 +18,7 @@ object ViewBindingCache {
     @Suppress("UNCHECKED_CAST")
     @RestrictTo(LIBRARY)
     internal fun <T : ViewBinding> getInflateWithLayoutInflater(viewBindingClass: Class<T>): InflateViewBinding<T> {
-        return inflateCache.getOrPut(viewBindingClass) {
-            InflateViewBinding(viewBindingClass)
-        } as InflateViewBinding<T>
+        return inflateCache.getOrPut(viewBindingClass) { InflateViewBinding(viewBindingClass) } as InflateViewBinding<T>
     }
 
     @Suppress("UNCHECKED_CAST")
@@ -41,19 +40,63 @@ object ViewBindingCache {
  * Wrapper of ViewBinding.inflate(LayoutInflater, ViewGroup, Boolean)
  */
 @RestrictTo(LIBRARY)
-internal class InflateViewBinding<out VB : ViewBinding>(viewBindingClass: Class<VB>) {
-
-    private val inflateViewBinding =
-        viewBindingClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
+internal abstract class InflateViewBinding<out VB : ViewBinding>(
+    private val inflateViewBinding: Method
+) {
 
     @Suppress("UNCHECKED_CAST")
-    fun inflate(layoutInflater: LayoutInflater): VB {
+    abstract fun inflate(layoutInflater: LayoutInflater): VB
+
+    @Suppress("UNCHECKED_CAST")
+    abstract fun inflate(layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean): VB
+}
+
+@RestrictTo(LIBRARY)
+@Suppress("FunctionName")
+internal fun <VB : ViewBinding> InflateViewBinding(viewBindingClass: Class<VB>): InflateViewBinding<VB> {
+    try {
+        val method = viewBindingClass.getMethod(
+            "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
+        )
+        return FullInflateViewBinding(method)
+    } catch (e: NoSuchMethodException) {
+        val method = viewBindingClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java)
+        return MergeInflateViewBinding(method)
+    }
+}
+
+@RestrictTo(LIBRARY)
+internal class FullInflateViewBinding<out VB : ViewBinding>(
+    private val inflateViewBinding: Method
+) : InflateViewBinding<VB>(inflateViewBinding) {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun inflate(layoutInflater: LayoutInflater): VB {
         return inflateViewBinding(null, layoutInflater, null, false) as VB
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun inflate(layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean): VB {
+    override fun inflate(layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean): VB {
         return inflateViewBinding(null, layoutInflater, parent, attachToParent) as VB
+    }
+}
+
+
+@RestrictTo(LIBRARY)
+internal class MergeInflateViewBinding<out VB : ViewBinding>(
+    private val inflateViewBinding: Method
+) : InflateViewBinding<VB>(inflateViewBinding) {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun inflate(layoutInflater: LayoutInflater): VB =
+        error("Specify parent to inflate ${InflateViewBinding::class.java.simpleName}")
+
+    @Suppress("UNCHECKED_CAST")
+    override fun inflate(layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean): VB {
+        require(attachToParent) {
+            "${InflateViewBinding::class.java.simpleName} supports inflate only with attachToParent=true"
+        }
+        return inflateViewBinding(null, layoutInflater, parent) as VB
     }
 }
 
